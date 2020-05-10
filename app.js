2006b93513fe96379de1fd2f3f85e0103c10a0ce
olivercolas <olivercolas.oc@gmail.com>
2020-05-10 19:06:59 +0100

    updated access allow origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
-        'https://practical-joliot-381616.netlify.com'
+        '*'
     )
     res.setHeader(
         'Access-Control-Allow-Headers',
